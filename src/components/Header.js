3aa1b43a783b6aecbf0e6ad84777968eb77ab929
Albert Kison <albert.kison@student.griffith.ie>
2021-10-12 15:00:51 +0100

    Get flight data

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,46 @@
 import { useState } from "react";
 
 function Header() {
+    const [destination, setDestination] = useState("")
+    const API_END_POINT = 'https://tequila-api.kiwi.com/v2/search'
+    const API_KEY = '2NiGrkLwod9HsPQi-CndW6zS8soBRNOI'
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        console.log(destination);
+
+        const params = {
+            "fly_from": "DUB",
+            "fly_to": "IEV",
+            "date_from": "01/11/2021",
+            "date_to": "02/01/2022"
+        };
+        const url = API_END_POINT + '?' + ( new URLSearchParams( params ) ).toString();
+        console.log(url);
+
+        fetch(url, {
+            headers: {
+                "apikey": API_KEY,
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+        })
+        .then(res => {
+            console.log(res);
+            return res.json();
+        })
+        .then(data => {
+            console.log(data);
+        })
+    }
+
     return (
         <div className="header">
             <div>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <label><strong>Where do you want to go?</strong></label><br />
-                    <input type="text" name="destination" placeholder="e.g. Paris" />
+                    <input type="text" name="destination" onChange={(e) => setDestination(e.target.value)} placeholder="e.g. Paris" />
                     <button type="submit">Go!</button>
                 </form>
             </div>
